ccf2cc3a7a7cd1f142de7587fc46c0a808dae1ff
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2023-01-15 21:41:03 +0000

    Submit btn check in

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,3 +4,8 @@ const answers = document.querySelectorAll('.answer')
 const modal = document.querySelector('.modal')
 const modalInfo = modal.querySelector('p')
 const modalBtn = modal.querySelector('.close-modal')
+
+const handleQuiz = e => {
+    e.preventDefault();
+}
+
