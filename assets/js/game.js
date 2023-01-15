69dd6e2b557070627931396804b1d094666859e5
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2023-01-15 21:21:48 +0000

    add main game commands

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,6 @@
- 
+const form =  document.querySelector('.quiz-box')
+const answers = document.querySelectorAll('.answer')
+
+const modal = document.querySelector('.modal')
+const modalInfo = modal.querySelector('p')
+const modalBtn = modal.querySelector('.close-modal')
