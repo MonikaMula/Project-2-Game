3336350f9fb194ab631caddf9b0b152c3c9fbf16
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2023-01-17 23:52:33 +0000

    errors correction

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,16 @@
 const form =  document.querySelector('.quiz-box')
-const answers = document.querySelectorAll('.answer')
-
+const answers = Array.from(document.querySelectorAll('.answer'))
+const allQuestions = document.querySelectorAll('question')
 const modal = document.querySelector('.modal')
 const modalInfo = modal.querySelector('p')
 const modalBtn = modal.querySelector('.close-modal')
 
+    
 const handleQuiz = e => {
-    e.preventDefault();
+        e.preventDefault();
 
     const checkedAnswers = answers.filter(answer => answer.checked)
-    const isTrue = checkedAnswers.evry(answerv=> answer.value ==='true')
+    const isTrue = checkedAnswers.every(answer => answer.value === 'true')
 
     const allChecked = checkedAnswers.length === allQuestions.length
 
@@ -17,6 +18,20 @@ const handleQuiz = e => {
         modal.classList.add('modal-active')
         modalInfo.textContent = 'Please Select All Answers'
     }
+
+    checkedAnswers.forEach(answer => {
+        const checkIfcorrect = answer.value === 'true'
+        const answerBox = answer.closest('.answer-box')
+
+        if (checkIfcorrect) {
+            answerBox.classList.add('correct')
+            answerBox.classList.remove('incorrect')
+        } else {
+            answerBox.classList.add('incorrect')
+            answerBox.classList.remove('correct')
+        }
+
+    })
 }
 
 const closeModal = () => {
