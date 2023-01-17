e83a01e1a41b847221f95170e6028fea7655ba87
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2023-01-17 20:11:17 +0000

    answers value check

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,8 +8,9 @@ const modalBtn = modal.querySelector('.close-modal')
 const handleQuiz = e => {
     e.preventDefault();
 
-    const checkedAnswers = answers.filter(answer => {
-        console.log(answer);
-        answer.checked})
+    const checkedAnswers = answers.filter(answer => answer.checked)
+    const isTrue = checkedAnswers.evry(answerv=> answer.value ==='true')
+
+    
 }
 
