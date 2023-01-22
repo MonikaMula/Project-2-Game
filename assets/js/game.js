65abfc31ce7b036f11cfac0ebf27ca1eb9677d80
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2023-01-22 21:28:36 +0000

    adding comments to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,4 @@
+// elemnts to quiz //
 const form = document.querySelector('.quiz-box')
 const answers = Array.from(document.querySelectorAll('.answer'))
 const allQuestions = document.querySelectorAll('.question')
@@ -5,19 +6,24 @@ const modal = document.querySelector('.modal')
 const modalInfo = modal.querySelector('p')
 const modalBtn = modal.querySelector('.close-modal')
 
+// function button-submit to handle the quiz //
 const handleQuiz = e => {
     e.preventDefault();
 
+// check what and if correct answers were picked //
     const checkedAnswers = answers.filter(answer => answer.checked)
     const isTrue = checkedAnswers.every(answer => answer.value === 'true')
-    const allChecked = checkedAnswers.length === allQuestions.length
 
+// making sure all questions are selected, if not info to select all answers //
+    const allChecked = checkedAnswers.length === allQuestions.length
+   
     if (!allChecked) {
         modal.classList.add('modal-active')
         modalInfo.textContent = 'Please Select All Answers!'
         return
     }
 
+// if all answers are selected, check if they are correct-chartreuse color or incorrect-crimson color //
     checkedAnswers.forEach(answer => {
         const checkIfCorrect = answer.value === 'true'
         const answerBox = answer.closest('.answer-box')
@@ -31,6 +37,7 @@ const handleQuiz = e => {
         }
     })
 
+// depending on correct or incorrect answers result congratulations or sorry //
     if (isTrue && allChecked) {
         modal.classList.add('modal-active')
         modalInfo.textContent = 'Congratulations!!👍 All Answers Are Correct🥳'
